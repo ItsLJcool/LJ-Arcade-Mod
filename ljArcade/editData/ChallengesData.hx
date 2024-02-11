9ac78fab85658860274bfce7a6aa6f65b586f0cc
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2024-02-11 09:48:08 -0700

    i forgor ok

--- ljArcade/editData/ChallengesData.hx
+++ ljArcade/editData/ChallengesData.hx
@@ -137,7 +137,6 @@ function update(elapsed:Float) {
         halfWay = true;
         challengeComplete({challengeID: 2});
     }
-    if (FlxG.keys.justPressed.B) EngineSettings.botplay = !EngineSettings.botplay;
 }
 
 function onNoteUpdatePost(note) {
